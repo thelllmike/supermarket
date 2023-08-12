199853957b55c0c2c7ef3a9bf84249766a3f4d1c
Sachin Harshitha <39330996+thelllmike@users.noreply.github.com>
2023-08-12 19:28:19 +0530

    update admin

--- src/cn/techtutorial/servlet/LoginServlet.java
+++ src/cn/techtutorial/servlet/LoginServlet.java
@@ -16,28 +16,29 @@ import cn.techtutorial.model.*;
 
 @WebServlet("/user-login")
 public class LoginServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8");
-		try (PrintWriter out = response.getWriter()) {
-			String email = request.getParameter("login-email");
-			String password = request.getParameter("login-password");
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String email = request.getParameter("login-email");
+            String password = request.getParameter("login-password");
 
-			UserDao udao = new UserDao(DbCon.getConnection());
-			User user = udao.userLogin(email, password);
-			if (user != null) {
-				request.getSession().setAttribute("auth", user);
-//				System.out.print("user logged in");
-				response.sendRedirect("index.jsp");
-			} else {
-				out.println("there is no user");
-			}
+            if ("admin@gmail.com".equals(email) && "password".equals(password)) {
+                // Simulate successful login for the admin
+                User admin = new User();
+                admin.setEmail(email);
+                admin.setPassword(password);
 
-		} catch (ClassNotFoundException|SQLException e) {
-			e.printStackTrace();
-		} 
+                request.getSession().setAttribute("auth", admin);
+                response.sendRedirect("admin.jsp");
+            } else {
+                out.println("Invalid credentials");
+            }
 
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
